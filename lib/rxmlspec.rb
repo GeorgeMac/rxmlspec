e2fa1154720bc10293df44b514dc75d83b7c4ef9
George MacRorie <george.macrorie@semantico.com>
2014-02-07 17:02:39 +0000

    Moved error function inside (note: this is going to be swapped out for user defined callback)

--- lib/rxmlspec.rb
+++ lib/rxmlspec.rb
@@ -2,15 +2,15 @@ require 'nokogiri'
 require 'rxmlspec/version'
 require 'rxmlspec/document'
 
-def error_function(message)
-  puts message
-end
-
 module Rxmlspec
 
   def validate(path, &block)
     doc = Nokogiri::XML(File.open(path, 'r'))
     Document.new(doc).instance_eval &block
+  end
+
+  def error_function(message)
+    puts message
   end
 
 end
