03b069cd1d483b299f110207763e8ee08d053740
George MacRorie <george.macrorie@semantico.com>
2014-02-10 15:46:20 +0000

    Updated mini test spec

--- test_spec.rb
+++ test_spec.rb
@@ -4,9 +4,9 @@ require 'bundler'
 Bundler.setup :default
 Bundler.require :default
 
-Rxmlspec::Validator.begin "test.xml" do
+Rxmlspec::Validator.begin "test.xml", err_func= ->(e){ puts ({message: e}) } do
   context "/stuff" do
-    permit ["a","b"]
+    permit ["a","b", "@c", "text(Herp Di Derple)"]
     expect "a" do
       permit "b" do
         permit "@c"
