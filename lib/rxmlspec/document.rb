8b2fe89fdc07225591163af8a9b67011bea74144
George MacRorie <george.macrorie@semantico.com>
2014-02-10 15:45:58 +0000

    Added some simple pattern matching on permit/exists methods string values

--- lib/rxmlspec/document.rb
+++ lib/rxmlspec/document.rb
@@ -20,8 +20,7 @@ class Document
       define_singleton_method :err_func, err_func
     end
 
-    def expect(path="", paths=[], &block)
-      paths << path
+    def expect(paths=[], &block)
       paths.each do |p|
         matches = @ctx.xpath(p)
         return err_func("The following match #{p} is required in the given context") if matches.to_a.empty?
@@ -29,11 +28,30 @@ class Document
       end
     end
 
-    def permit(path="", paths=[], &block)
+    def permit(paths=[], &block)
       present = @ctx.children.map { |child| child.name() }
-      paths << path
+      puts "Components: #{resolve_components_on_context(paths)}"
       return err_func("The following elements: #{present - paths} are not valid") if (present - paths).length > 0
       present.each { |p| Context.new(p, @err_func).instance_eval &block } unless block.nil?
+    end
+
+    private
+    def resolve_components_on_context(components)
+      result = Hash.new {|h,k| h[k] = []}
+      text = /text\((?<content>.*?)\)/
+      attr = /\@(?<content>.*)/
+      components.map do |item|
+        case item
+        when text
+          result[:text] << text.match(item)[:content]
+        when attr
+          match = attr.match(item)[:content].split(":")
+          result[:attr] << {key: match[0], value: match[1]}
+        else
+          result[:elem] << item
+        end
+      end
+      return result
     end
 
   end
